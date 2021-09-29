906b99e07c0dea446f635de420c04821577f77fb
April John Maraat <aprilmaraat@gmail.com>
2021-09-29 19:12:01 +0800

    Os version and publish configuration

--- Ti9.API/Program.cs
+++ Ti9.API/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Ti9.API
 {
@@ -14,6 +15,14 @@ namespace Ti9.API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //var host = new WebHostBuilder()
+            //.UseKestrel()
+            //.UseContentRoot(Directory.GetCurrentDirectory())
+            //.UseIISIntegration()
+            //.UseStartup<Startup>()
+            //.Build();
+
+            //host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
